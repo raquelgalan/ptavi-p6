616614190dcf1d09dd0229a037c2950834456743
raquel galan montes <rgalan@iota11.aulas.gsyc.es>
2014-11-24 12:42:17 +0100

    client pep8

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
+# Raquel Galán Montes
 """
 Programa cliente que abre un socket a un servidor
 """
@@ -7,57 +8,52 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
-
+# Verificar los argumentos
 try:
 
     METODO = sys.argv[1]
-    
+
     if METODO != "INVITE" and METODO != "BYE":
         raise SystemExit
-        
+
     LOGIN = sys.argv[2]
     IP_PORT = LOGIN.split("@")[1]
-    PORT = int(LOGIN.split(":")[1]) 
-    print PORT
+    PORT = int(LOGIN.split(":")[1])
     SERVER = IP_PORT.split(":")[0]
-    print SERVER
 
-#except ValueError:
-#    print ("Error: El puerto necesita ser entero")
-
-#except IndexError:
 except:
     print ("Usage: python client.py method receiver@IP:SIPport")
     raise SystemExit
 
-
 # Contenido que vamos a enviar
-
 RECEPTOR = LOGIN.split("@")[0]
 
 LINE = METODO + " sip:" + RECEPTOR + "@" + SERVER + " SIP/2.0"
+
+# Verificar el servidor y el puerto
 try:
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
+    # Si el argumento 1 es invite o bye se envia el contenido
     print "Enviando: " + LINE
-    my_socket.send(LINE + '\r\n')
+    my_socket.send(LINE + '\r\n\r\n')
     data = my_socket.recv(1024)
 except socket.error:
     print ("Error: No server listening at " + SERVER + " port " + str(PORT))
     raise SystemExit
-#sip = " SIP/2.0 "
-print 'Recibido -- \r\n', data
-if data == " SIP/2.0 100 Trying\r\n" + " SIP/2.0 180 Ring\r\n" + " SIP/2.0 200 OK\r\n":
+s = "SIP/2.0 "
+e = "\r\n\r\n"
+print 'Recibido -- \r\n\r\n', data
+
+# Si se recibe 100, 180 y 200 se envia ACK con el contenido
+if data == s + "100 Trying" + e + s + "180 Ringing" + e + s + "200 OK" + e:
     METODO = "ACK"
     LINE = METODO + " sip:" + RECEPTOR + "@" + SERVER + " SIP/2.0"
-    my_socket.send(LINE + '\r\n')
+    print "Enviando: " + LINE
+    my_socket.send(LINE + '\r\n\r\n')
 
-    
 print "Terminando socket..."
 
 # Cerramos todo
